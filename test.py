80b9d4db1522ac7a014dfbfcc9ea3761ccb1dec4
ikaushalmp <trikcykaushal@gmail.com>
2024-05-15 10:15:59 +0530

    This is the version for new website

---
+++ test.py
@@ -0,0 +1,11 @@
+function_change=1
+def function_one():
+    print("Function One called")
+
+def function_two():
+    print("Function Two called")
+
+# Assuming you have some loop here
+while(True):
+
+  
